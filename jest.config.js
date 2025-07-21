ff393495c25c423cde3c05c8f07b4bd118523f1a
eoussama <oussama_gtacj@live.fr>
2025-07-21 22:29:53 +0100

    chore: add Jest configuration file for TypeScript testing

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+      tsconfig: "tsconfig.json",
+    },
+  },
+  moduleNameMapper: {
+    "^(\.{1,2}/.*)\\.js$": "$1",
+  },
+};
