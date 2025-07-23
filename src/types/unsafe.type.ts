fe55e9de5b10efc6f5f6221ed9ffeb437ceee17e
eoussama <oussama_gtacj@live.fr>
2025-07-23 22:01:40 +0100

    fix: corrected unsafe typing

--- src/types/unsafe.type.ts
+++ src/types/unsafe.type.ts
@@ -1,8 +1,13 @@
+import type { TNullable } from "./nullable.type";
+
+
+
 /**
  * @description
  * Unsafe type
+ * The type infers the value as nullable or undefined
  *
  * @param T - The type to make unsafe
  * @returns The unsafe type
  */
-export type TUnsafe<T> = T | undefined;
+export type TUnsafe<T> = TNullable<T> | undefined;
